0d370837125067b78ec0b8203ac373e2e57e726f
Daehwan Nam <dhnam@postech.ac.kr>
2023-01-15 18:56:46 +0900

    cuda files are renamed

--- static/gsai-cluster/cuda/cuda-11.6.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/sh
-
-export PATH=/home/dhnam/usr/local/cuda-11.6/bin:$PATH
-export LD_LIBRARY_PATH=/home/dhnam/usr/local/cuda-11.6/lib64:$LD_LIBRARY_PATH
---
+++ static/gsai-cluster/gpu/cuda-11.6.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/sh
+
+export PATH=/home/${USER}/usr/local/cuda-11.6/bin:$PATH
+export LD_LIBRARY_PATH=/home/${USER}/usr/local/cuda-11.6/lib64:$LD_LIBRARY_PATH
