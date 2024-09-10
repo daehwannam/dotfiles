a1c149e3c9ac87db59d77ac92664bb24957b9350
Daehwan Nam <dhnam@postech.ac.kr>
2024-09-10 13:39:09 +0900

    mpv config update

--- static/common/.emacs.d/config/init/dependent/search-engines.lisp
+++
@@ -1,2 +0,0 @@
-(("postech-calendar" "https://www.postech.ac.kr/academics/academic-info/academic-calendar/" "https://www.postech.ac.kr/academics/academic-info/academic-calendar/")
- ("poscal" "https://www.postech.ac.kr/academics/academic-info/academic-calendar/" "https://www.postech.ac.kr/academics/academic-info/academic-calendar/"))
