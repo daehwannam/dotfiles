5f5a2e24fc65bebe17ff724b3b1fcd36c5a30c6a
Daehwan Nam <dhnam@postech.ac.kr>
2026-09-12 13:33:33 +0900

    firefox update

---
+++ dynamic/librewolf/link.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+THIS_PATH=`realpath $0`
+DIR_PATH=`dirname $THIS_PATH`
+
+ARGS=$1
+PROFILE_PATH_FILE_PATH=$DIR_PATH/profile-path.txt
+
+touch -a $PROFILE_PATH_FILE_PATH
+
+profile_path=$(cat $PROFILE_PATH_FILE_PATH)
+
+if [ -z "${profile_path}" ]; then
+    echo "The profile path should be specified in \"$PROFILE_PATH_FILE_PATH\"."
+    echo 'Open URL "about:profiles" to check the path.'
+    exit 1
+fi
+
+
+stow $ARGS -t $profile_path -d $DIR_PATH profile-config-default
+# echo "stow $ARGS -t $profile_path -d $DIR_PATH profile-config-betterfox"
